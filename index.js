f721f7a40b2300b259b47465bab5a41b267f8b89
Ilkka Korhonen <99470756+ilkkaKorhonenFuturiot@users.noreply.github.com>
2024-04-03 15:07:53 +0300

    to internet

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 var morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(express.static('dist'))
 
 let persons = {
     "persons": [
@@ -102,7 +107,7 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
